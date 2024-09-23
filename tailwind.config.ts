3034efb8738b373a166ec451ce4275d0ba419e02
Esutarosa <esutarosa.git@gmail.com>
2024-09-23 21:03:57 +0300

    style: add tailwind nextui styles and container config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,23 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/theme";
 
 const config: Config = {
   content: [
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "3xl": "1400px",
+      },
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [nextui()],
 };
 
 export default config;
