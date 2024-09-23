8ac0ad2b6aa6689ce6f509e0cc27518582d74546
Esutarosa <esutarosa.git@gmail.com>
2024-09-23 21:21:08 +0300

    feat: add skeleton for home component

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,8 +1,13 @@
 import type { FC } from "react";
 import Layout from "@/components/layouts/layout";
+import { Home } from "@/components/home";
 
 const HomePage: FC = () => {
-  return <Layout>HomePage</Layout>;
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  );
 };
 
 export default HomePage;
