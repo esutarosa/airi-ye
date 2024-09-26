e742d60f683ecb26b361bc426d86f6cd20468e38
Esutarosa <esutarosa.git@gmail.com>
2024-09-26 18:49:09 +0300

    feat: add backgroud for site and course page

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -1,7 +1,17 @@
 import type { FC, PropsWithChildren } from "react";
 
 const SiteLayout: FC<PropsWithChildren> = ({ children }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <div className="fixed left-0 top-0 -z-10 h-full w-full">
+        <div className="relative h-full w-full bg-black">
+          <div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+          <div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div>
+        </div>
+      </div>
+      {children}
+    </>
+  );
 };
 
 export default SiteLayout;
---
+++ src/app/(site)/third/page.tsx
@@ -0,0 +1,12 @@
+import type { FC } from "react";
+import Layout from "@/components/layouts/layout";
+
+const HomePage: FC = () => {
+  return (
+    <Layout>
+      <div>Third page</div>
+    </Layout>
+  );
+};
+
+export default HomePage;
