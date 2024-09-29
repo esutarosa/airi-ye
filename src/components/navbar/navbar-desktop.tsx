1fc54d7a46269eda6c6b197c3b4382d45d835d28
Esutarosa <esutarosa.git@gmail.com>
2024-09-29 23:44:07 +0300

    refactor: improve sidebar header

--- src/components/navbar/navbar-desktop.tsx
+++ src/components/navbar/navbar-desktop.tsx
@@ -1,5 +1,4 @@
 import type { FC } from "react";
-import { Logo } from "@/components/logo";
 import Link from "next/link";
 import { FiGithub } from "react-icons/fi";
 
@@ -7,7 +6,6 @@ const NavbarDesktop: FC = () => {
   return (
     <div className="container flex h-14 items-center border-b border-content1">
       <div className="flex w-full items-center justify-between">
-        <Logo />
         <nav className="flex items-center space-x-4">
           <Link href="https://github.com/Esutarosa/airi-ye" target="_blank">
             <FiGithub className="size-5" />
--- src/components/sidebar/sidebar-header.tsx
+++ src/components/sidebar/sidebar-header.tsx
@@ -1,19 +1,21 @@
 import type { FC } from "react";
-import { Button } from "@nextui-org/button";
+
 import { Link } from "@nextui-org/link";
+import { Badge } from "@nextui-org/badge";
+import { Tent } from "lucide-react";
 
 const SidebarHeader: FC = () => {
   return (
     <div className="flex h-14 items-center justify-between border-b border-content1 px-4 lg:h-[60px] lg:px-6">
-      <Link
-        href="/"
-        className="flex items-center gap-2 font-semibold text-foreground"
-      >
-        AIRI
-      </Link>
-      <Button radius="full" variant="flat" isIconOnly aria-label="Notification">
-        NF
-      </Button>
+      <Badge content="beta" size="sm" className="-right-5 border-none">
+        <Link
+          className="text-xl flex items-center gap-1 text-foreground font-semibold tracking-wider"
+          href="/"
+        >
+          <Tent className="w-5 h-5" />
+          AIRI
+        </Link>
+      </Badge>
     </div>
   );
 };
