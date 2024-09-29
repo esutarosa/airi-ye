a5e235e4167b531821fa8e92f59cd020892c5215
Esutarosa <esutarosa.git@gmail.com>
2024-09-29 23:52:41 +0300

    refactor: improve navbar

--- src/components/navbar/navbar-desktop.tsx
+++ src/components/navbar/navbar-desktop.tsx
@@ -4,14 +4,12 @@ import { FiGithub } from "react-icons/fi";
 
 const NavbarDesktop: FC = () => {
   return (
-    <div className="container flex h-14 items-center border-b border-content1">
-      <div className="flex w-full items-center justify-between">
-        <nav className="flex items-center space-x-4">
-          <Link href="https://github.com/Esutarosa/airi-ye" target="_blank">
-            <FiGithub className="size-5" />
-          </Link>
-        </nav>
-      </div>
+    <div className="w-full flex h-14 items-center border-b border-content1">
+      <nav className="flex w-full justify-end items-end space-x-4">
+        <Link href="https://github.com/Esutarosa/airi-ye" target="_blank">
+          <FiGithub className="size-5" />
+        </Link>
+      </nav>
     </div>
   );
 };
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -3,7 +3,7 @@ import { NavbarDesktop } from "@/components/navbar";
 
 const Navbar: FC = () => {
   return (
-    <header className="w-full">
+    <header className="flex h-14 items-center gap-4 border-b border-content2 bg-content1/70 px-4 lg:h-[60px] lg:px-6">
       <NavbarDesktop />
     </header>
   );
