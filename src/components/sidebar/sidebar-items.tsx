f9c775faa7f2a3c0bffeb7a31f536c24b723bf47
Esutarosa <esutarosa.git@gmail.com>
2024-09-30 00:50:39 +0300

    fix: corrected hover effect on active item in the sidebar

--- src/components/sidebar/sidebar-items.tsx
+++ src/components/sidebar/sidebar-items.tsx
@@ -18,7 +18,8 @@ const SidebarItems: FC = () => {
             href={item.href}
             className={cn(
               "text-foreground/50 gap-3 rounded-lg transition-all hover:text-foreground hover:bg-content1 px-3 py-2",
-              pathname === item.href && "bg-foreground text-background",
+              pathname === item.href &&
+                "bg-content2/70 text-foreground hover:bg-content2/70 hover:opacity-100",
             )}
           >
             {item.icon}
