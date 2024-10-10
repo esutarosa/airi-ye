38f1d128e9941b7b52718e3943670f9296b8de5c
esutarosa <esutarosa.git@gmail.com>
2024-10-10 23:43:05 +0300

    rearrange(auth): clear auth config

--- src/app/(landing)/homework/page.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from "react";
-import { Layout } from "@/components/layouts";
-
-const HomeworkPage: FC = () => {
-  return <Layout>HomeworkPage</Layout>;
-};
-
-export default HomeworkPage;
--- src/app/(landing)/schedule/page.tsx
+++
@@ -1,8 +0,0 @@
-import type { FC } from "react";
-import { Layout } from "@/components/layouts";
-
-const SchedulePage: FC = () => {
-  return <Layout>SchedulePage</Layout>;
-};
-
-export default SchedulePage;
