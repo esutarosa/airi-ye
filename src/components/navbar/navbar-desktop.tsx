b50f8a6b2405cf1f9364b5a7ddafe2715fe5abe7
esutarosa <esutarosa.git@gmail.com>
2024-10-16 00:12:36 +0300

    build: update github icon

--- src/components/navbar/navbar-desktop.tsx
+++ src/components/navbar/navbar-desktop.tsx
@@ -1,13 +1,13 @@
 import type { FC } from "react";
 import Link from "next/link";
-import { FiGithub } from "react-icons/fi";
+import { Github } from "lucide-react";
 
 const NavbarDesktop: FC = () => {
   return (
     <div className="w-full flex h-14 items-center">
       <nav className="flex w-full justify-end items-end space-x-4">
         <Link href="https://github.com/Esutarosa/airi-ye" target="_blank">
-          <FiGithub className="size-5" />
+          <Github className="size-5" />
         </Link>
       </nav>
     </div>
