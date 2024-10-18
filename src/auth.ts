6b514403f2e582feb28804ba0051ef07edcbdbe8
esutarosa <esutarosa.git@gmail.com>
2024-10-18 22:16:25 +0300

    feat(auth): add drizzle adapter

--- src/auth.ts
+++ src/auth.ts
@@ -4,8 +4,11 @@ import { safeParse } from "valibot";
 import { AuthFormSchema } from "@/definitions/auth-form-schema";
 import { findUserByEmail } from "@/actions/resources/user-queries";
 import argon2 from "argon2";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import db from "@/db/drizzle";
 
 const nextAuth = NextAuth({
+  adapter: DrizzleAdapter(db),
   session: { strategy: "jwt" },
   secret: process.env.NEXTAUTH_SECRET,
   pages: { signIn: "/auth" },
